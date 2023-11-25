5f727eabd63a28ed117fb7035e578176cf6bc5ba
cpbots_PedroSaidtg <trigo.villalpando.pedro@cbtis159.edu.mx>
2023-11-24 20:28:42 -0600

    act
    lo intente

--- src/main/java/frc/robot/subsystems/intecchasis.java
+++ src/main/java/frc/robot/subsystems/intecchasis.java
@@ -7,14 +7,21 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class intecchasis extends SubsystemBase {
   private final TalonSRX m_360Srx=new TalonSRX(3);
 
   public intecchasis() {}
-public void Intec(XboxController controlK){
+public void Intec(CommandXboxController controlk2){
+m_360Srx.set(lefttr, 1);
+  
+
+
 
 }
 
